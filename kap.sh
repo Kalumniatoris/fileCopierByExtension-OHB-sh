9d8e5c03c0138465dba36c12b92595314146b124
Dominik Kwiatkowski <kalumniatoris@gmail.com>
2011-12-15 22:10:00 +0100

    Umieszczono na gicie, pierwsza umieszczona wersja programu kopiujacego odpowiednio rozszerzone pliki

---
+++ kap.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "Chcesz skopiowaæ wszystkie pliki z rozszerzeniem $1 do folderu $2"\
+
+if [ -d $2 ]; then
+	echo folder nie jest tworzony gdy¿ istnieje
+	else
+	mkdir $2
+fi
+
+#touch tmpsrc.roz
+#touch tmpdes.roz
+
+find . -iname "*.$1" > tmpsrc.roz
+sed -e 's|^./||' tmpsrc.roz > tmpdes.roz
+sed -e 's|[^//]*$||' tmpdes.roz > tmp.roz
+cat tmp.roz > tmpdes.roz
+rm tmp.roz ##
+
+plik=`head tmpsrc.roz -n 1`
+gdzie=`head tmpdes.roz -n 1`
+while [ -s tmpsrc.roz ]; do
+echo $gdzie
+sed 1d tmpsrc.roz>tmp.roz
+cat tmp.roz>tmpsrc.roz
+rm tmp.roz ##
+sed 1d tmpdes.roz>tmp.roz
+cat tmp.roz>tmpdes.roz
+if [ -d ./$2/$gdzie ]; then
+	echo "--||--  $plik"
+	else
+	mkdir -p ./$2/$gdzie
+fi
+cp $plik ./$2/$gdzie
+plik=`head tmpsrc.roz -n 1`
+gdzie=`head tmpdes.roz -n 1`
+done
+rm tmpsrc.roz
+rm tmpdes.roz
+
+
+
