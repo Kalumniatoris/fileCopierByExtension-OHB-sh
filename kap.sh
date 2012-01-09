23b4ba20568cd65b843e6fd86bb22f38a8145b05
Kalumniatoris <kalumniatoris@gmail.com>
2012-01-09 14:15:03 +0100

    a

--- kap.sh
+++ kap.sh
@@ -7,19 +7,17 @@ else
 echo "Chcesz skopiowac wszystkie pliki z rozszerzeniem $1 do folderu $2"\
 
 if [ -d "$2" ]; then
-	echo folder nie jest tworzony gdyc istnieje
+	echo folder nie jest tworzony gdyz istnieje
 	else
 	mkdir "$2"
 fi
 
-#touch tmpsrc.roz
-#touch tmpdes.roz
 
 find . -iname "*.$1" > tmpsrc.roz
 sed -e 's|^./||' tmpsrc.roz > tmpdes.roz
 sed -e 's|[^//]*$||' tmpdes.roz > tmp.roz
 cat tmp.roz > tmpdes.roz
-#rm tmp.roz ##
+
 
 plik=`head tmpsrc.roz -n 1`
 gdzie=`head tmpdes.roz -n 1`
@@ -27,7 +25,7 @@ while [ -s tmpsrc.roz ]; do
 echo "$gdzie"
 sed 1d tmpsrc.roz>tmp.roz
 cat tmp.roz>tmpsrc.roz
-#rm tmp.roz ##
+
 sed 1d tmpdes.roz>tmp.roz
 cat tmp.roz>tmpdes.roz
 if [ -d "./$2/$gdzie" ]; then
